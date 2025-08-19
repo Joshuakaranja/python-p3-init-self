5c22e81bfbedd72e49619c63950e7502a30853ce
Joshua Karanja <jokaranja26@gmail.com>
2025-08-19 14:22:54 +0300

    initialzie commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+# manual test block
+if __name__ == "__main__":
+    fido = Dog("Fido", "Dalmatian")
+    spot = Dog("Spot")  # should default to "Mutt"
+    
+    print(fido.name, fido.breed)  # Fido Dalmatian
+    print(spot.name, spot.breed)  # Spot Mutt
+
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-
 class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+# manual test block
+if __name__ == "__main__":
+    guido = Person("Guido")
+    print(guido.name)  # Guido
+
     pass
